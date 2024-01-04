8b6f6581f70b1c2b2cb88727c2bb2fc51e83a7e0
superhadtr <superhadtr@gmail.com>
2024-01-04 22:07:34 +0300

    GameManager+

--- Pac-Man Deneme - Copy/Assets/Scripts/GameManager.cs
+++ Pac-Man Deneme - Copy/Assets/Scripts/GameManager.cs
@@ -1,18 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Mob[] mob;
+    public Pacman Pacman;
+    public Transform Coins;
+
+    public int score { get; private set; }
+    public int lives { get; private set; }
+
+    private void Start()
     {
-        
+        NewGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(this.lives <=0 && Input.anyKeyDown)
+        {
+            NewGame();
+        }
+    }
+
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+        NewRound();
+    }
+
+    //Temel oyun kurallarýyla ilgili fonksiyonlar
+
+    private void NewRound()
+    {
+        foreach (Transform coin in this.Coins)
+        {
+            coin.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < this.mob.Length; i++)
+        {
+            this.mob[i].gameObject.SetActive(false);
+        }
+        this.Pacman.gameObject.SetActive(false);
+    }
+
+    private void ResetState()
+    {
+        for (int i = 0; i < this.mob.Length; i++)
+        {
+            this.mob[i].gameObject.SetActive(true);
+        }
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+
+    public void MobEaten(Mob mob)
+    {
+        SetScore(this.score + Mob.Points);
+    }
+
+    public void PacmanEaten()
+    {
+        this.Pacman.gameObject.SetActive(false);
+        if (this.lives > 0)
+        {
+            //Öldükten sonra delay
+            Invoke(nameof(ResetState), 3f);
+            ResetState();
+        }
+        else { GameOver (); }
     }
 }
+    
+
